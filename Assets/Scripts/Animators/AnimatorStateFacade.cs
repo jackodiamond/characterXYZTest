fbcb030099216916a18cd4d78b5d40dca4c14d9c
abhishek <abhishekkumaryadav@anymedi.com>
2024-03-22 13:02:34 +0530

    animation scripts added

---
+++ Assets/Scripts/Animators/AnimatorStateFacade.cs
@@ -0,0 +1,33 @@
+﻿using Characters;
+using Characters.Movement;
+using Characters.Movement.Behaviours;
+using UnityEngine;
+
+namespace Animators
+{
+    public class AnimatorStateFacade : MonoBehaviour
+    {
+    //    private IAttackManager _attackManager;
+        private Animator _animator;
+        private ICharacter _character;
+
+        private void Start()
+        {
+            _character = GetComponentInParent<ICharacter>();
+            _animator = GetComponent<Animator>();
+        }
+
+        private void MakeSureCharacterIsNotNull()
+        {
+            _character = _character ?? GetComponentInParent<ICharacter>();
+        }
+
+        private void OnAnimatorMove()
+        {
+            MakeSureCharacterIsNotNull();
+            var movement = _character.getMovement();
+            if (!_animator || !(movement is AttackingMovement) || !(movement is IRootMotion)) return;
+            ((IRootMotion) movement).SetRootMotionAdditionalPosition(_animator.deltaPosition);
+        }
+    }
+}
---
+++ Assets/Scripts/Animators/ICharacterAnimator.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+
+namespace Animators
+{
+    public interface ICharacterAnimator
+    {
+        Animator getAnimator();
+        void SetHorInput(float horInput);
+        void SetVerInput(float verInput);
+        void SetInputMagnitude(float inputMagnitude);
+        void SetGroundVelocity(float groundVelocity);
+        void SetIsFalling(bool isFalling);
+        void SetIsAboutToLand(bool isAboutToLand);
+        void SetJumping(bool jumping);
+        void SetCrouching(bool crouching);
+        void SetUnskippable(bool unskippable);
+        void SetSliding(bool sliding);
+        void SetRunning(bool running);
+    }
+}
